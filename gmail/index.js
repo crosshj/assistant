41718e514a796eda79f3b7d32a9ff2b78b634b62
Cross, Harrison (CORP) <Harrison.Cross@adp.com>
2018-02-08 19:37:52 -0500

    more decoding

--- gmail/index.js
+++ gmail/index.js
@@ -1,5 +1,6 @@
 var passport = require('passport');
 var Gmail = require('node-gmail-api');
+var gapi= require('googleapis');
 var config = require('../auth/config');
 const cheerio = require('cheerio');
 var _ = require('lodash');
@@ -16,32 +17,61 @@ https://console.developers.google.com/apis/credentials
 
 
 */
+var accessToken = config.accessToken;
+var gmail = new Gmail(accessToken);
+
+function decode64(string){
+    return new Buffer(string, 'base64').toString('binary');
+}
+
+function getAttachments(userId, message, callback) {
+    var parts = message.payload.parts;
+    for (var i = 0; i < parts.length; i++) {
+        var part = parts[i];
+        if (part.filename && part.filename.length > 0) {
+        var attachId = part.body.attachmentId;
+        const getConfig = {
+            'id': attachId,
+            'messageId': message.id,
+            'userId': userId
+        };
+        console.log(getConfig);
+        callback();
+        // var request = gapi.client.gmail.users.messages.attachments.get(getConfig);
+        // request.execute(function(attachment) {
+        //     callback(part.filename, part.mimeType, attachment);
+        // });
+        }
+    }
+}
 
 function getMessages() {
-    var accessToken = config.accessToken;
-    var gmail = new Gmail(accessToken);
+
 
     // in:T S O
-    const query = new Buffer('aW46VHJhc2ggU3BlY2lhbCBPZmZlcg==', 'base64').toString('binary');
+    const query = decode64('aW46VHJhc2ggU3BlY2lhbCBPZmZlcg==');
 
     var s = gmail.messages(query, {format: 'full', max: 100});
-    var allParts = [];
+    var allMessages = [];
     var partsWithoutAttachment = [];
-    s.on('data', function (d) {
+    s.on('data', function (message) {
         //console.log(Object.keys(d.payload))
         //console.log(d.payload.body)
-        allParts = allParts.concat(d.payload.parts);
+        allMessages = allMessages.concat(message);
     });
     s.on('end', function(){
-        console.log(`--- stream ended, allparts.length = ${allParts.length} `);
-        allParts.forEach(part => {
-            if(part.filename){
-                //https://developers.google.com/gmail/api/v1/reference/users/messages/attachments/get
-                //TODO: fetch attachment here
-                console.log(`\t${part.filename}`);
-            } else {
-                partsWithoutAttachment.push(part);
-            }
+        console.log(`--- stream ended, allMessages.length = ${allMessages.length} `);
+        allMessages.forEach(message => {
+            //https://developers.google.com/gmail/api/v1/reference/users/messages/attachments/get
+            getAttachments(config.userId, message, (filename, mimeType, attachment) => {
+                console.log(`got attachment: ${filename}`);
+            });
+
+            message.payload.parts.forEach(part => {
+                if(!part.filename){
+                    partsWithoutAttachment.push(part);
+                }
+            });
         });
         console.log(`--- done with filenames, parts without attachments: ${partsWithoutAttachment.length}`);
 
@@ -51,8 +81,15 @@ function getMessages() {
             var partParts = thisPart.parts;
             if(partParts && partParts.length){
                 partParts.forEach(part => {
-                    console.log('--- wtf dunno');
-                    //console.log(part.body.data)
+                    const body = decode64(part.body.data);
+                    const $ = cheerio.load(body);
+                
+                    $('a').each(function(i, elem) {
+                        const linkText = $(this).text();
+                        if(linkText.includes('http')){
+                            links.push(linkText);
+                        }
+                    });
                 })
             } else {
                 //mimeType: text/html
