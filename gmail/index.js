43fd9ac48e0cb93726d8894508236abfac244155
HJ Cross <github@chimpjuice.com>
2018-03-06 20:26:39 -0500

    refactor main

--- gmail/index.js
+++ gmail/index.js
@@ -13,27 +13,20 @@ if(config.proxy){
     process.env.HTTP_PROXY = config.proxy;
 }
 
-
-function main() {
-    // in:T S O
-    //const query = decode64('aW46VHJhc2ggU3BlY2lhbCBPZmZlcg==');
-    const query = 'to:note@chimpjuice.com OR to:note@crosshj.com';
-
-    
-    async.waterfall([
-        callback => getMessages({query}, callback),
-        getParts,
-        getPartsWrapped,
-        getResults
-    ], (err, results) => {
-        if(err){
-            return console.log({err});
-        }
-        const which = 10;
-        console.log(results[which]((e,r)=>{ console.log({e,r})}))
-    });
-}
+// in:T S O
+//const query = decode64('aW46VHJhc2ggU3BlY2lhbCBPZmZlcg==');
+const query = 'to:note@chimpjuice.com OR to:note@crosshj.com';
 
 
-console.log('--- app starting up');
-main();
+async.waterfall([
+    callback => getMessages({query}, callback),
+    getParts,
+    getPartsWrapped,
+    getResults
+], (err, results) => {
+    if(err){
+        return console.log({err});
+    }
+    const which = 10;
+    console.log(results[which]((e,r)=>{ console.log({e,r})}))
+});
