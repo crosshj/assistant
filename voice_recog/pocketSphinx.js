b075742e7ae365273fff8572d85432fe69455329
Harrison Cross <harrison.cross@adp.com>
2019-07-18 23:42:24 -0400

    voice recognition: install/test pocketsphinx, try google

---
+++ voice_recog/pocketSphinx.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+
+var ps = require('pocketsphinx').ps;
+
+modeldir = "./pocketsphinx/model/en-us/"
+
+var config = new ps.Decoder.defaultConfig();
+config.setString("-hmm", modeldir + "en-us");
+config.setString("-dict", modeldir + "cmudict-en-us.dict");
+config.setString("-lm", modeldir + "en-us.lm.bin");
+//config.setString("-logfn", "/dev/null");
+var decoder = new ps.Decoder(config);
+
+const fileToRead = "./sample.wav";
+const testFile = "./pocketsphinx/test/data/goforward.raw";
+fs.readFile(fileToRead, function(err, data) {
+    if (err) throw err;
+    decoder.startUtt();
+    decoder.processRaw(data, false, false);
+		decoder.endUtt();
+		const { prob, bestScore, hypstr } = decoder.hyp();
+    console.log({ prob, bestScore, hypstr })
+});
